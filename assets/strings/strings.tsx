e175cc7784af2d86921991f194ed8955c427513b
meenakshi-kumari-v <meenakshikr1999@gmail.com>
2025-06-13 17:36:21 +0530

    Create strings.tsx

---
+++ assets/strings/strings.tsx
@@ -0,0 +1,29 @@
+const Strings = {
+  send_noti: 'Send Notification',
+  userNm: 'Username',
+  password: 'Password',
+  login: 'Login',
+  ok: 'Ok',
+  no: 'No',
+  yes: 'Yes',
+  m: 'Male',
+  f: 'Female',
+  
+  //screenNAme
+  profile: 'Profile',
+  leav_stat: 'Leave Status',
+  payslp: 'Payslip',
+  benefit: 'Benefits',
+  holdy_Calen: 'Holiday Calendar',
+  com_polc: 'Company Policies',
+  cont_inf: 'Contact Matrix',
+  esi_epf: 'ESI/EPF Details',
+  infra: 'Infra Details',
+  esi_fam_det: 'Family Details',
+  notifitn: 'Notification',
+  idcard: 'ID Card',
+  setng: 'Settings',
+  prvyPol: 'Privacy Policy',
+
+};
+export default Strings;
